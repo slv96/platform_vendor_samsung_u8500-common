3fe8aace11559d72f2a26a2412330d4c5c906a18
Shantanu Londhe <shantanulondhe26@gmail.com>
2013-06-21 12:51:54 +0530

    Add Goo IM auto-updater

---
+++ version.mk
@@ -0,0 +1,11 @@
+# Goo IM Updater
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.modversion=TeamCanjica-$(BUILD_VERSION)-$(PRODUCT_DEVICE)-$(BUILD_ID) \
+    ro.developerid=$(DEVELOPER) \
+    ro.goo.developerid=$(DEVELOPER) \
+    ro.goo.rom=cm_$(TARGET_PRODUCT) \
+    ro.goo.version=$(BUILD_VERSION) \
+    ro.goo.board=$(PRODUCT_DEVICE)
+    
+# ro.developerid should not change or it will cause problems. we have to fix one account where the releases would be uploaded.
+# perhaps create an account on goo.im with username as teamcanjica ?
