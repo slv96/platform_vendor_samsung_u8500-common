a7cb6c4370b26c1fbefeabfd3e5b26b5a5a01e39
Pablo Lunarenko <kimiaropablo958@gmail.com>
2013-06-21 11:01:18 -0300

    version.mk: ifequals
    for some reason (as not being an environment variable) we need to define ro.goo.board in other way, we circunvent this problem with the already defined envar UPLOAD_FOLDER from Jenkins, this also can be done with "else", but just to get it bugfree, I did it this way.
    
    We modify the modversion a little so it reflects the proper envars

--- version.mk
+++ version.mk
@@ -1,11 +1,19 @@
 # Goo IM Updater
+ifeq ($(UPLOAD_FOLDER),janice)
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=TeamCanjica-$(BUILD_VERSION)-$(PRODUCT_DEVICE)-$(BUILD_ID) \
+    ro.goo.board=GT-I9070
+endif
+ifeq ($(UPLOAD_FOLDER),janicep)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.board=GT-I9070P
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.modversion=TeamCanjica-$(BUILD_VERSION)-$(LUNCH)-$(BUILD_NUMBER) \
     ro.developerid=$(DEVELOPER) \
     ro.goo.developerid=$(DEVELOPER) \
-    ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=$(BUILD_NUMBER) \
-    ro.goo.board=$(PRODUCT_DEVICE)
+    ro.goo.rom=TeamCanjica-$(TARGET_PRODUCT) \
+    ro.goo.version=$(BUILD_NUMBER)
     
 # ro.developerid should not change or it will cause problems. we have to fix one account where the releases would be uploaded.
 # perhaps create an account on goo.im with username as teamcanjica ?
