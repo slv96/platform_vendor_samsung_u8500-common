1f7af02fc01de3f95ee84d5c12339fa563b89f9e
Diego Porto <diego.cfporto@gmail.com>
2013-06-29 18:45:38 -0300

    fix for GooManager notification update
    since we are doing nightlies it's better to use a date based version id.
    that way we'll always have a higher id number (integer) on each build, just like goo.im wants it.

--- version.mk
+++ version.mk
@@ -13,7 +13,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.developerid=$(DEVELOPER) \
     ro.goo.developerid=$(DEVELOPER) \
     ro.goo.rom=TeamCanjica-$(TARGET_PRODUCT) \
-    ro.goo.version=$(BUILD_NUMBER)
+    ro.goo.version=$(shell date +%s)
     
 # ro.developerid should not change or it will cause problems. we have to fix one account where the releases would be uploaded.
 # perhaps create an account on goo.im with username as teamcanjica ?
