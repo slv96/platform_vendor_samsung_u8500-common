68644a2d303b55f87cb680e4c100628bab7b2803
Rox-Ace2 <rooobertinhooo@gmail.com>
2013-10-26 01:01:39 +0200

    Fix GPS

--- codina/codina-vendor-blobs.mk
+++ codina/codina-vendor-blobs.mk
@@ -89,6 +89,8 @@ PRODUCT_COPY_FILES += \
 
 # GPS
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/system/bin/gps.cer:system/bin/gps.cer \
+    $(LOCAL_PATH)/system/bin/gpsd:system/bin/gpsd \
     $(LOCAL_PATH)/system/etc/AGPS_CA.pem:system/etc/AGPS_CA.pem \
     $(LOCAL_PATH)/system/etc/gps.xml:system/etc/gps.xml \
     $(LOCAL_PATH)/system/lib/hw/gps.montblanc.so:system/lib/hw/gps.montblanc.so
