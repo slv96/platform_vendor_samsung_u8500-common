7a863102de829b0c97904eb68ee501d077991a46
Salvatore Battaglia <salvo.96bat@gmail.com>
2014-06-11 11:04:30 +0200

    Update janice vendor to ITV I9070XXLQL
    Signed-off-by: Salvatore Battaglia <salvo.96bat@gmail.com>

--- janice/janice-vendor-blobs.mk
+++ janice/janice-vendor-blobs.mk
@@ -145,6 +145,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/lib/hw/audio.primary.montblanc.so:system/lib/hw/audio.primary.montblanc.so \
     $(LOCAL_PATH)/system/lib/hw/audio_policy.montblanc.so:system/lib/hw/audio_policy.montblanc.so \
     $(LOCAL_PATH)/system/lib/libaudiopolicy_sec.so:system/lib/libaudiopolicy_sec.so \
+    $(LOCAL_PATH)/system/lib/libaudiosa.so:system/lib/libaudiosa.so \
     $(LOCAL_PATH)/system/lib/lib_Samsung_SB_AM_for_ICS_v03008.so:system/lib/lib_Samsung_SB_AM_for_ICS_v03008.so \
     $(LOCAL_PATH)/system/lib/lib_SamsungRec_V01006.so:system/lib/lib_SamsungRec_V01006.so \
     $(LOCAL_PATH)/system/lib/libbassapp.so:system/lib/libbassapp.so \
